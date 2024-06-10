bd30f16b66c4815cf725a86285aab35845cc6444
Valentin <guillaumevalentin76@gmail.com>
2024-06-10 11:12:50 +0200

    correction de la route patch => gestion du bouléen

--- build/routes/task.js
+++ build/routes/task.js
@@ -29,7 +29,7 @@ router.post("/task", async (req, res) => {
 });
 router.patch("/task/:id", async (req, res) => {
     try {
-        let finished = Boolean(req.query.finished);
+        let finished = req.query.finished === "true" ? true : false;
         const { id } = req.params;
         const task = await Task.findByIdAndUpdate(id, {
             finished: finished,
